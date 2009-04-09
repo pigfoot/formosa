a8da06af8427c60bfae70d755e04eb36363ea71f
Zhong, Ming-Xun <zhongmx@gmail.com>
2009-04-09 23:11:29 +0800

    Add more keyboard define

--- src/tsbbs.h
+++ src/tsbbs.h
@@ -59,19 +59,40 @@ struct one_key {           /* Used to pass commands to the readmenu */
 /*******************************************************************
  *	key define
  *******************************************************************/
-#define KEY_TAB		9
-#define KEY_UP		0x0101
-#define KEY_DOWN	0x0102
-#define KEY_RIGHT	0x0103
-#define KEY_LEFT	0x0104
+#define KEY_TAB         9
+#define KEY_ESC         27
+#define KEY_CR		('\r')	    // Ctrl('M'), 0x0D
+#define KEY_LF		('\n')	    // Ctrl('J'), 0x0A
+#define KEY_ENTER	KEY_CR	    // for backward compatibility
+#define KEY_BS		(Ctrl('H'))
+#define KEY_BS2		(0x7f)
 
-#define KEY_HOME	0x0201
-#define KEY_INS		0x0202
-#define KEY_DEL		0x0203
-#define KEY_END		0x0204
-#define KEY_PGUP	0x0205
-#define KEY_PGDN	0x0206
+#define KEY_UP          0x0101
+#define KEY_DOWN        0x0102
+#define KEY_RIGHT       0x0103
+#define KEY_LEFT        0x0104
+#define KEY_STAB        0x0105	/* shift-tab */
 
+#define KEY_HOME        0x0201
+#define KEY_INS         0x0202
+#define KEY_DEL         0x0203
+#define KEY_END         0x0204
+#define KEY_PGUP        0x0205
+#define KEY_PGDN        0x0206
+
+#define KEY_F1		0x0301
+#define KEY_F2		0x0302
+#define KEY_F3		0x0303
+#define KEY_F4		0x0304
+#define KEY_F5		0x0305
+#define KEY_F6		0x0306
+#define KEY_F7		0x0307
+#define KEY_F8		0x0308
+#define KEY_F9		0x0309
+#define KEY_F10		0x030A
+#define KEY_F11		0x030B
+#define KEY_F12		0x030C
+#define KEY_UNKNOWN	0x0FFF	/* unknown sequence */
 
 /*******************************************************************
  * 	other define or macro
