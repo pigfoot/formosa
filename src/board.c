bd49d075da2281638ea16b543f75cfce4a613a45
Guo-Fu Tseng <cooldavid@cooldavid.org>
2009-04-28 13:33:56 +0800

    Change unread symbol.
    zmx say "+" is too big.. XD

--- src/board.c
+++ src/board.c
@@ -86,7 +86,7 @@ static void board_entry(int x, void *ep, int idx, int top, int last, int rows)
 			       (show_numposts && in_board) ? be1->binfr->numposts : num,
 			       (ReadRC_Board(be1->bhr->filename,
 					     be1->bhr->bid,
-					     curuser.userid)) ? "[1;31m+[m" : "",
+					     curuser.userid)) ? "[1;31m.[m" : "",
 			       be1->bhr->filename,
 			       (be1->bhr->brdtype & BRD_NEWS) ? _msg_board_3 : "",
 #ifdef USE_IDENT
